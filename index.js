8296b631e8d70f4484639f5d9dcf1ba831428daa
Seohee Jung <seoheejung561@gmail.com>
2020-10-27 02:32:01 -0400

    backend server is set up

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv').config();
+const config = require('./config/key');
+
+const bodyParser = require('body-parser');
+const cookParser = require('cookie-parser');
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('mongoDB connected'))
+	.catch(err => console.error(err));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
 
 const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send('Hello');
+});
 
 app.listen(port, () => {
 	console.log(`Server running at ${port}`);
